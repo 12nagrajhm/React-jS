e487df4c66a424bf1a0f682fddfeb076f7e8a2df
Nagraj <nagrajhm2002@gmail.com>
2025-03-03 12:10:54 +0530

    Forth comit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,13 @@
 import Form from "./component/Form";
 import Greet from "./component/Greet"
 import Initial from "./component/Initial"
-
+import Random from "./component/Random"
 const App=()=>{
   return <div>
     <Greet/>
     <Initial/>
     <Form/>
+    <Random/>
   </div>
 };
 
--- my-react-app/src/component/Form.jsx
+++ my-react-app/src/component/Form.jsx
@@ -6,6 +6,8 @@ const Form=()=>{
             <input type="name" id="name" name="name" required /><br/>
             <label for="email">Email</label>
             <input type="email" is="email" name="email"/><br/>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password"/>
             <button type="Submit">Submit</button>
         </form>
 
